4f6cb9d955fc69043b561666bbde2ebccbcf52f2
Overgrown <overgrownmc@gmail.com>
2024-11-21 15:30:40 -0500

    Added Docs site

--- js/template/dark-day-mode-toggle.js
+++ js/template/dark-day-mode-toggle.js
@@ -1,14 +1,23 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
     const button = document.querySelector(".moon-button, .sun-button");
+    const buttonIcon = button.querySelector("img");
+
     if (document.body.classList.contains("dark-mode")) {
-        button.innerHTML = "☀";
+        buttonIcon.src = document.body.classList.contains("dark-mode") ? "assets/template/images/navigation_bar/sun.svg" : "assets/template/images/navigation_bar/moon.svg";
+        buttonIcon.style.filter = "brightness(0.8)";
+        buttonIcon.alt = "Sun Icon";
         button.classList.remove("moon-button");
         button.classList.add("sun-button");
     } else {
-        button.innerHTML = "☾";
+        buttonIcon.src = document.body.classList.contains("dark-mode") ? "assets/template/images/navigation_bar/sun.svg" : "assets/template/images/navigation_bar/moon.svg";
+        buttonIcon.style.filter = "brightness(0.8)";
+        buttonIcon.alt = "Moon Icon";
         button.classList.remove("sun-button");
         button.classList.add("moon-button");
     }
 }
-document.getElementById("year").textContent = new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("year").textContent = new Date().getFullYear();
+});
